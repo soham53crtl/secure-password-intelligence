0a5316d112b717508c4fda76fab2ebadf1aa59e8
soham53crtl <sohamroychoudhury08@gmail.com>
2026-04-23 12:44:22 +0530

    Add PasswordRequest class with getter and setter

---
+++ PasswordRequest.java
@@ -0,0 +1,6 @@
+public class PasswordRequest {
+    private String password;
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
